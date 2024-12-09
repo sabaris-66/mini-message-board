3f06308a57ea14ae274dad8a7a5812c622a1bd25
Sabaris <sabaris.elango@gmail.com>
2024-12-09 18:16:36 +0530

    Express Server

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+const indexRouter = require("./routes/indexRouter");
+const path = require("node:path");
+
+app.use(express.urlencoded({ extended: true }));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use("/", indexRouter);
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send(err.message);
+});
+
+app.listen(PORT, () => console.log("Listening on Port 3000"));
